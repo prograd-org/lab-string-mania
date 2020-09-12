e657284951ecd04fc364ddd05fcc0b7d0b2468df
surajdesai1297@gmail.com <suraj@LAPTOP-C83DOH9J.localdomain>
2020-09-12 22:00:13 +0530

    FSJ0820B107

--- src/utility/StringMethod.java
+++ src/utility/StringMethod.java
@@ -1,3 +1,47 @@
 package utility;
 
 // Type your code
+
+public class StringMethod {
+	
+	private String input;
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+
+	
+	public String lower(String input) {
+		// will change to lowercase 
+		return input.toLowerCase();
+	}
+	
+	public String upper(String input) {
+		// will change to uppercase
+		return input.toUpperCase();
+	}
+	
+    public String firstCharacter(String input) {
+    	// will return the first character in string 
+		
+		return Character.toString(input.charAt(0));
+	}
+    
+    public String length(String input) {
+    	// will return the length of string
+    	
+    	return Integer.toString(input.length());
+    }
+    
+    public String trim(String input) {
+    	// it will removing the trailing spaces from start and end of string.
+    	return input.trim();
+    }
+	
+	
+}
