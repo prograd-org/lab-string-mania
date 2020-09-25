8883568573d7276cc512d97566b81d6f9c3bcf6b
Ravikachhadiya <ravikachhadiya@gmail.com>
2020-09-25 17:23:45 +0530

    FSJ0720A115

--- src/testing/TestStringAdvanceMethod.java
+++ src/testing/TestStringAdvanceMethod.java
@@ -1,6 +1,5 @@
 package testing;
 
-/* Uncomment the below code to test your application
 
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -63,4 +62,4 @@ public class TestStringAdvanceMethod {
 		}
 	}
 }
-*/
+
--- src/utility/StringAdvanceMethod.java
+++ src/utility/StringAdvanceMethod.java
@@ -1,3 +1,44 @@
 package utility;
 
-// Type your code
+import java.util.Arrays;
+
+public class StringAdvanceMethod{
+	
+	String input1;
+	String input2;
+	public String getInput1() {
+		return input1;
+	}
+	public void setInput1(String input1) {
+		this.input1 = input1;
+	}
+	public String getInput2() {
+		return input2;
+	}
+	public void setInput2(String input2) {
+		this.input2 = input2;
+	}
+	
+	public String concat(String s1, String s2) {
+		try {
+			s1.concat(s2).toString();
+			
+		}
+		catch(Exception e){
+			return "";
+		}
+		return s1.concat(s2).toString();
+	}
+	
+	public String split(String s1, String s2) {
+		String[] s = s1.split(s2);
+		String s3 = "  ".concat(s[1]);
+		String ot = s[0].concat(s3);
+		System.out.print(ot);
+		return ot;
+	}
+	
+	public String indexOf(String s1, String s2) {
+		return String.valueOf(s1.indexOf(s2));
+	}
+}
