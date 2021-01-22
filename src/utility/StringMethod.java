75ef7255b484a726cc17f90226eadb74f8380125
harinimanu <harinimanu123@gmail.com>
2021-01-22 16:35:04 +0530

    1688889

--- src/utility/StringMethod.java
+++ src/utility/StringMethod.java
@@ -1,3 +1,33 @@
 package utility;
 
-// Type your code
+public class StringMethod{
+	
+	String input;
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+	public String lower(String s) {
+		return s.toLowerCase();
+	}
+	
+	public String upper(String s) {
+		return s.toUpperCase();
+	}
+	
+	public String firstCharacter(String s) {
+		return String.valueOf(s.charAt(0));
+	}
+	public String length(String s) {
+		
+		return String.valueOf(s.length());
+	}
+	public String trim(String s) {
+		return s.trim();
+	}
+}
